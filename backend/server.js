5f387761691b65ae74a4466a7995e51b3cc56f8a
trevormandela <mandelatrevor@gmail.com>
2024-09-14 17:13:02 +0300

    Final Commit

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,9 @@ app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV == 'production') {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    })
+    });
 }
 
 app.listen(PORT, () => {
